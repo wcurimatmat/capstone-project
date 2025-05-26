40d278e9b00752bdd014e8587b254f9b14b01bab
wcurimatmat <wcurimatmat24@gmail.com>
2025-05-26 08:38:31 +0800

    modify vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/scss/index.css", "resources/js/app.jsx"],
+            input: ["resources/scss/index.scss", "resources/js/app.jsx"],
             refresh: true,
         }),
         tailwindcss(),
