e07c4945ccfed2a341bdd8f1773580404347962f
wcurimatmat <wcurimatmat24@gmail.com>
2025-06-05 14:00:04 +0800

    feat: add catalog component

---
+++ resources/js/Pages/Web/Shop/Catalog/Catalog.jsx
@@ -0,0 +1,47 @@
+import styles from "./Catalog.module.scss";
+import ProductCard from "../../../../Components/Shared/ProductCard/ProductCard";
+
+function Catalog() {
+    return (
+        <ul className={styles.Catalog}>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+            <li className={styles.Catalog__catalogItem}>
+                <ProductCard />
+            </li>
+        </ul>
+    );
+}
+
+export default Catalog;
