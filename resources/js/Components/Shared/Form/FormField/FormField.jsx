415882a2571f0537eaa5718e2534cb376c5af3d8
wcurimatmat <wcurimatmat24@gmail.com>
2025-06-16 10:38:18 +0800

    feat: add form field component

---
+++ resources/js/Components/Shared/Form/FormField/FormField.jsx
@@ -0,0 +1,15 @@
+import styles from "./FormField.module.scss";
+import PropTypes from "prop-types";
+
+function FormField({ id, label, children }) {
+    return (
+        <div className={styles.FormField}>
+            <label htmlFor={id} className={styles.FormField__formLabel}>
+                {label}
+            </label>
+            {children}
+        </div>
+    );
+}
+
+export default FormField;
