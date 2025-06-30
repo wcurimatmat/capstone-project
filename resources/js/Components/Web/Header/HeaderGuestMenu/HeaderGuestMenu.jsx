bed276d31ad6d4b45a7c159d8b2c2e908d116300
wcurimatmat <wcurimatmat24@gmail.com>
2025-06-30 15:27:03 +0800

    feat: add header guest menu component

--- resources/js/Components/Web/Header/HeaderGuestMenu/HeaderGuestMenu.jsx
+++ resources/js/Components/Web/Header/HeaderGuestMenu/HeaderGuestMenu.jsx
@@ -1,66 +1,34 @@
-// icons
-import { XIcon, SignInIcon, UserPlusIcon } from "@phosphor-icons/react";
+// assets
+import { SignInIcon, UserPlusIcon } from "@phosphor-icons/react";
 
 // styles
 import styles from "./HeaderGuestMenu.module.scss";
 
-function HeaderGuestMenu({ userActionState, setUserActionState }) {
-    
-    
+function HeaderGuestMenu() {
     return (
-        <div className={styles.HeaderGuestMenu}>
-            <div
-                className={`${
-                    userActionState
-                        ? styles.HeaderGuestMenu___menuOverlayActive
-                        : ""
-                }`}
-            ></div>
-            <div
-                className={`${styles.HeaderGuestMenu__guestMenu} ${
-                    userActionState ? styles.HeaderGuestMenu___menuActive : ""
-                }`}
-            >
-                <div className={styles.HeaderGuestMenu__mainWrapper}>
-                    <div
-                        className={
-                            styles.HeaderGuestMenu__menuCloseButtonWrapper
-                        }
-                    >
-                        <button
-                            onClick={() => setUserActionState(!userActionState)}
+        <div className="HeaderGuestMenu">
+            <nav className="HeaderGuestMenu__menuNav">
+                <ul className="HeaderGuestMenu__menuNavList">
+                    <li className={styles.HeaderGuestMenu__menuNavItem}>
+                        <a
+                            href={route("auth.create")}
+                            className={styles.HeaderGuestMenu__menuNavLink}
                         >
-                            <XIcon />
-                        </button>
-                    </div>
-                    <nav className={styles.HeaderGuestMenu__menuNav}>
-                        <ul className={styles.HeaderGuestMenu__menuNavList}>
-                            <li className={styles.HeaderGuestMenu__menuNavItem}>
-                                <a
-                                    href={route("auth.create")}
-                                    className={
-                                        styles.HeaderGuestMenu__menuNavLink
-                                    }
-                                >
-                                    <SignInIcon size={20} />
-                                    <p>Sign-In</p>
-                                </a>
-                            </li>
-                            <li className={styles.HeaderGuestMenu__menuNavItem}>
-                                <a
-                                    href={route("auth.store")}
-                                    className={
-                                        styles.HeaderGuestMenu__menuNavLink
-                                    }
-                                >
-                                    <UserPlusIcon size={20} />
-                                    <p>Sign-Up</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
+                            <SignInIcon size={20} />
+                            <p>Sign-In</p>
+                        </a>
+                    </li>
+                    <li className={styles.HeaderGuestMenu__menuNavItem}>
+                        <a
+                            href={route("auth.store")}
+                            className={styles.HeaderGuestMenu__menuNavLink}
+                        >
+                            <UserPlusIcon size={20} />
+                            <p>Sign-Up</p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
