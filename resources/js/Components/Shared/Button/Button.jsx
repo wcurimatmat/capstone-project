476191478fd6dd89add772883bac293dcc08342c
wcurimatmat <wcurimatmat24@gmail.com>
2025-06-30 12:44:43 +0800

    feat: add button component

---
+++ resources/js/Components/Shared/Button/Button.jsx
@@ -0,0 +1,17 @@
+//styles
+import styles from "./Button.module.scss";
+
+function Button({ children, variant = "primary", ...props }) {
+    const variantClasses = {
+        primary: styles.Button___primaryButton,
+        secondary: styles.Button___secondaryButton,
+        tertiary: styles.Button___tertiaryButton,
+        cart: styles.Button__productCardButton,
+    }
+
+    const variantClass = variantClasses[variant] || variantClasses.primary;
+
+    return <button className={`${variantClass}`} {...props}>{children}</button>;
+}
+
+export default Button;
