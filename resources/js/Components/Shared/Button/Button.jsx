327868378c1654a1fc626da0fd4f58943407126c
wcurimatmat <wcurimatmat24@gmail.com>
2025-06-30 13:31:51 +0800

    modify: update Button component by adding className props

--- resources/js/Components/Shared/Button/Button.jsx
+++ resources/js/Components/Shared/Button/Button.jsx
@@ -1,17 +1,21 @@
 //styles
 import styles from "./Button.module.scss";
 
-function Button({ children, variant = "primary", ...props }) {
+function Button({ children, variant = "primary", className = "", ...props }) {
     const variantClasses = {
         primary: styles.Button___primaryButton,
         secondary: styles.Button___secondaryButton,
         tertiary: styles.Button___tertiaryButton,
         cart: styles.Button__productCardButton,
-    }
+    };
 
     const variantClass = variantClasses[variant] || variantClasses.primary;
 
-    return <button className={`${variantClass}`} {...props}>{children}</button>;
+    return (
+        <button className={`${variantClass} ${className}`} {...props}>
+            {children}
+        </button>
+    );
 }
 
 export default Button;
