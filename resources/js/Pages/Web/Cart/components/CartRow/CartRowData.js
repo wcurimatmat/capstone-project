7454b314355be46a7df6ce386102b19a2bd61129
wcurimatmat <wcurimatmat24@gmail.com>
2025-07-09 14:25:06 +0800

    feat: add a script file that populates the cart row component

---
+++ resources/js/Pages/Web/Cart/components/CartRow/CartRowData.js
@@ -0,0 +1,32 @@
+import MockItemImage from "/resources/assets/visuals/item-6.jpg";
+
+export const data = [
+    {
+        image: MockItemImage,
+        name: "Green Dress",
+        price: 100.5,
+        size: "Medium",
+        color: "Green",
+    },
+    {
+        image: MockItemImage,
+        name: "Green Dress",
+        price: 100.5,
+        size: "Medium",
+        color: "Green",
+    },
+    {
+        image: MockItemImage,
+        name: "Green Dress",
+        price: 100.5,
+        size: "Medium",
+        color: "Green",
+    },
+    {
+        image: MockItemImage,
+        name: "Green Dress",
+        price: 100.5,
+        size: "Medium",
+        color: "Green",
+    },
+];
