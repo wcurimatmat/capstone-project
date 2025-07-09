a1da738bd017afef493812f493fa669cf5712795
wcurimatmat <wcurimatmat24@gmail.com>
2025-07-09 14:26:04 +0800

    modify: add route name for shop page route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::get("/", function () {
     return inertia("Web/Home/Index");
 });
 
-Route::inertia("/shop", "Web/Shop/Index/Index");
+Route::inertia("/shop", "Web/Shop/Index/Index")->name("shop.index");
 Route::inertia("/about", "Web/About/Index");
 
 Route::inertia("/shop/item-name", "Web/Shop/Show/Show");
