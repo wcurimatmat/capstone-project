36a4868d1f73b4e596f23d613cb3fe4904735f59
wcurimatmat <wcurimatmat24@gmail.com>
2025-07-13 11:23:06 +0800

    fix: UserDropdownMenu
    - Rename JSX component from HeaderUserMenu to UserDropdownMenu
    - Rename stylesheet from HeaderUserMenu to UserDropdownMenu

---
+++ resources/js/Components/User/Header/components/UserDropdownMenu/UserDropdowMenu.jsx
@@ -0,0 +1,61 @@
+// assets
+import { XIcon } from "@phosphor-icons/react";
+
+// styles
+import styles from "./HeaderUserActionMenu.module.scss";
+
+// components
+import Button from "../../../../Shared/Button/Button";
+import HeaderUserMenu from "../HeaderUserMenu/HeaderUserMenu";
+import HeaderGuestMenu from "../GuestMenu/HeaderGuestMenu";
+
+function DropdownMenuWrapper({ isUserAuthenticated }) {
+    return (
+        <div className="HeaderUserActionMenu__menuContentWrapper">
+            {isUserAuthenticated ? <HeaderUserMenu /> : <HeaderGuestMenu />}
+        </div>
+    );
+}
+
+function UserDropdownMenu({
+    userActionState,
+    setUserActionState,
+    isUserAuthenticated,
+}) {
+    return (
+        <>
+            {/* menu backdrop */}
+            {userActionState && (
+                <div
+                    className={styles.MenuBackdrop}
+                    onClick={() => setUserActionState(false)}
+                ></div>
+            )}
+
+            <div
+                className={`${styles.UserDropdownMenu} ${
+                    userActionState ? styles.UserDropdownMenu__isMenuActive : ""
+                }`}
+                id="header-user-menu"
+            >
+                {/* menu wrapper */}
+                <div className="UserDropdownMenu__menuWrapper">
+                    {/* menu toggler */}
+                    <div className={styles.UserDropdownMenu__menuToggleBlock}>
+                        <Button
+                            variant="icon"
+                            onClick={() => setUserActionState(false)}
+                        >
+                            <XIcon size={24} />
+                        </Button>
+                    </div>
+
+                    {/* menu content */}
+                    <DropdownMenuWrapper isUserAuthenticated={isUserAuthenticated} />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default UserDropdownMenu;
