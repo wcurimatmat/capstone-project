e2129223f4907f27b63f559068e565ef045ace81
wcurimatmat <wcurimatmat24@gmail.com>
2025-07-13 10:43:23 +0800

    fix: fix parent path related to the pages for users. from Web to User

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
-    return inertia("Web/Home/Index");
+    return inertia("User/Home/Index");
 })->name("home");
-Route::inertia("/about", "Web/About/Index")->name("about");
+Route::inertia("/about", "User/About/Index")->name("about");
 
-Route::inertia("/catalogs", "Web/Catalog/Index/Index")->name("catalog.index");
-Route::inertia("/catalogs/item-name", "Web/Catalog/Show/Show")->name("catalog.show");
+Route::inertia("/catalogs", "User/Catalog/Index/Index")->name("catalog.index");
+Route::inertia("/catalogs/item-name", "User/Catalog/Show/Show")->name("catalog.show");
 
-Route::inertia("/cart", "Web/Cart/Index")->name("cart.index");
+Route::inertia("/cart", "User/Cart/Index")->name("cart.index");
 
 // Auth
 
-Route::inertia("/login", "Web/Auth/Login/Create")->name("login");
-Route::inertia("/register", "Web/Auth/Register/Store")->name("register");
+Route::inertia("/login", "User/Auth/Login/Create")->name("login");
+Route::inertia("/register", "User/Auth/Register/Store")->name("register");
 
 // admin panel
 
