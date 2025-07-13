5b71601b4455ec7f9c1f981d645d7ee675606162
wcurimatmat <wcurimatmat24@gmail.com>
2025-07-14 07:22:32 +0800

    chore: rename directory from NavMobile to MobileNavigation

---
+++ resources/js/Components/User/Header/components/MobileNavigation/NavMobile.jsx
@@ -0,0 +1,59 @@
+import { XIcon } from "@phosphor-icons/react";
+import styles from "./NavMobile.module.scss";
+
+function NavMobile({ mobileMenuState, setMobileMenuState }) {
+    return (
+        <nav
+            className={`${styles.NavMobile} ${
+                mobileMenuState ? styles.NavMobile___active : ""
+            }`}
+        >
+            <div className={styles.NavMobile__wrapper}>
+                <div className={styles.NavMobile__mobileNavButtonWrapper}>
+                    <button
+                        onClick={() => setMobileMenuState(!setMobileMenuState)}
+                    >
+                        <XIcon size={24} />
+                    </button>
+                </div>
+
+                <ul className={styles.NavMobile__navigationList} role="list">
+                    <li className={styles.NavMobile__navigationItem}>
+                        <a
+                            href="#"
+                            className={styles.NavMobile__navigationLink}
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li className={styles.NavMobile__navigationItem}>
+                        <a
+                            href="#"
+                            className={styles.NavMobile__navigationLink}
+                        >
+                            Shop
+                        </a>
+                    </li>
+                    <li className={styles.NavMobile__navigationItem}>
+                        <a
+                            href="#"
+                            className={styles.NavMobile__navigationLink}
+                        >
+                            About
+                        </a>
+                    </li>
+                    <li className={styles.NavMobile__navigationItem}>
+                        <a
+                            href="#"
+                            className={styles.NavMobile__navigationLink}
+                        >
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default NavMobile;
