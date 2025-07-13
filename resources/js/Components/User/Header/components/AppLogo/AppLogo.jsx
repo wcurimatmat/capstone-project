306cce668c9e280177491206689d4f0b3aa9c356
wcurimatmat <wcurimatmat24@gmail.com>
2025-07-13 10:50:30 +0800

    chore: rename jsx file from NavAppLogo to AppLogo

---
+++ resources/js/Components/User/Header/components/AppLogo/AppLogo.jsx
@@ -0,0 +1,18 @@
+import AppLogo from "../../../../../assets/logo/rjs.png";
+import styles from "./AppLogo.module.scss";
+
+function AppLogo() {
+    return (
+        <figure className={styles.AppLogo}>
+            <a href="">
+                <img
+                    src={AppLogo}
+                    alt="app header logo"
+                    className={styles.AppLogo__appLogo}
+                />
+            </a>
+        </figure>
+    );
+}
+
+export default AppLogo;
