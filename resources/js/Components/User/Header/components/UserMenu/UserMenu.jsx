e83ed72ad8092504d8526175b31123e192af158f
wcurimatmat <wcurimatmat24@gmail.com>
2025-07-14 08:17:09 +0800

    refactor: change block identifier class name from HeaderUserMenu to UserMenu

--- resources/js/Components/User/Header/components/UserMenu/UserMenu.jsx
+++ resources/js/Components/User/Header/components/UserMenu/UserMenu.jsx
@@ -13,64 +13,49 @@ import styles from "./UserMenu.module.scss";
 // assets
 import MockAvatar from "/resources/assets/avatar/pexels-soldiervip-1391498.jpg";
 
-function HeaderUserMenu() {
+function UserMenu() {
     return (
-        <div className="HeaderUserMenu">
-            <div className={styles.HeaderUserMenu__user}>
+        <div className="UserMenu">
+            <div className={styles.UserMenu__user}>
                 <figure>
                     <img
                         src={MockAvatar}
                         alt=""
-                        className={styles.HeaderUserMenu__userAvatar}
+                        className={styles.UserMenu__userAvatar}
                     />
                 </figure>
 
                 <p>Maria Llewelyn</p>
             </div>
 
-            <nav className={styles.HeaderUserMenu__menuNav}>
-                <ul className={styles.HeaderUserMenu__menuNavList}>
-                    <li className={styles.HeaderUserMenu__menuNavItem}>
-                        <a
-                            href=""
-                            className={styles.HeaderUserMenu__menuNavLink}
-                        >
+            <nav className={styles.UserMenu__menuNav}>
+                <ul className={styles.UserMenu__menuNavList}>
+                    <li className={styles.UserMenu__menuNavItem}>
+                        <a href="" className={styles.UserMenu__menuNavLink}>
                             <UserIcon size={20} />
                             <span>Account</span>
                         </a>
                     </li>
-                    <li className={styles.HeaderUserMenu__menuNavItem}>
-                        <a
-                            href=""
-                            className={styles.HeaderUserMenu__menuNavLink}
-                        >
+                    <li className={styles.UserMenu__menuNavItem}>
+                        <a href="" className={styles.UserMenu__menuNavLink}>
                             <AddressBookIcon size={20} />
                             <span>Address</span>
                         </a>
                     </li>
-                    <li className={styles.HeaderUserMenu__menuNavItem}>
-                        <a
-                            href=""
-                            className={styles.HeaderUserMenu__menuNavLink}
-                        >
+                    <li className={styles.UserMenu__menuNavItem}>
+                        <a href="" className={styles.UserMenu__menuNavLink}>
                             <BellIcon size={20} />
                             <span>Notifications</span>
                         </a>
                     </li>
-                    <li className={styles.HeaderUserMenu__menuNavItem}>
-                        <a
-                            href=""
-                            className={styles.HeaderUserMenu__menuNavLink}
-                        >
+                    <li className={styles.UserMenu__menuNavItem}>
+                        <a href="" className={styles.UserMenu__menuNavLink}>
                             <ShoppingBagIcon size={20} />
                             <span>Orders</span>
                         </a>
                     </li>
-                    <li className={styles.HeaderUserMenu__menuNavItem}>
-                        <a
-                            href=""
-                            className={styles.HeaderUserMenu__menuNavLink}
-                        >
+                    <li className={styles.UserMenu__menuNavItem}>
+                        <a href="" className={styles.UserMenu__menuNavLink}>
                             <SignOutIcon size={20} />
                             <span>Logout</span>
                         </a>
@@ -81,4 +66,4 @@ function HeaderUserMenu() {
     );
 }
 
-export default HeaderUserMenu;
+export default UserMenu;
