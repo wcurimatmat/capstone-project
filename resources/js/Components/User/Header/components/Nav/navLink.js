d51b75dd65061aa9bc32d30dd72fd9e70d32975d
wcurimatmat <wcurimatmat24@gmail.com>
2025-07-14 08:34:08 +0800

    feat: add navLinks data

---
+++ resources/js/Components/User/Header/components/Nav/navLink.js
@@ -0,0 +1,20 @@
+const navLinks = [
+    {
+        name: "Home",
+        url: route("home"),
+    },
+    {
+        name: "Catalogs",
+        url: route("catalog.index"),
+    },
+    {
+        name: "About",
+        url: route("about"),
+    },
+    {
+        name: "Contact",
+        url: "#",
+    },
+];
+
+export default navLinks;
